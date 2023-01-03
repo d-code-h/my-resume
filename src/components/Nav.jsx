7d6819af8171dfb90d39d2faf13fc04a1b1c006e
Yunusa David <daveed856@outlook.com>
2023-01-03 10:48:55 +0100

    Nav becomes responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import style from '../styles/Nav.module.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { useEffect } from 'react';
 
 export default function Nav() {
   const navLinks = [
@@ -13,23 +14,40 @@ export default function Nav() {
     'Contact',
   ];
   const [showLinks, setShowLinks] = useState(false);
+  const [display, setDisplay] = useState(false);
+  const handleMenuClick = () => {
+    setShowLinks(!showLinks);
+    setDisplay(!display);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 600) {
+      setShowLinks(false);
+      setDisplay(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [showLinks, display]);
+
   return (
     <nav className={style.nav}>
       {showLinks ? (
-        <FaTimes
-          className={style.nav__bar}
-          onClick={() => setShowLinks(!showLinks)}
-        />
+        <FaTimes className={style.nav__bar} onClick={() => handleMenuClick()} />
       ) : (
-        <FaBars
-          className={style.nav__bar}
-          onClick={() => setShowLinks(!showLinks)}
-        />
+        <FaBars className={style.nav__bar} onClick={() => handleMenuClick()} />
       )}
-      <div className={style.nav__menu}>
+      <div className={display ? style.nav__menu_open : style.nav__menu}>
         {navLinks.map((link) => {
           return (
-            <a href={`#${link.toLowerCase()}`} className={style.nav__link}>
+            <a
+              key={link}
+              href={`#${link.toLowerCase()}`}
+              className={style.nav__link}
+            >
               {link}
             </a>
           );
